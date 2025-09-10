ba263bcc7ae79a5cd1b79b947c51abb45b007e07
JT <JT@UndergroundBunker.co.uk>
2025-09-10 10:06:14 +0100

    test: add poorly formatted test file to verify pre-commit hook

---
+++ test_formatting.dart
@@ -0,0 +1,11 @@
+// Test file with poor formatting
+class TestClass {
+  String name;
+  int age;
+
+  TestClass(this.name, this.age);
+
+  String toString() {
+    return 'TestClass{name: $name, age: $age}';
+  }
+}
